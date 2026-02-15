578648b55fad91b5ccf4d1f617236348a81aaad6
morgan-mcnabb <morgan.mcnabb@protonmail.com>
2026-02-15 15:46:36 -0500

    fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'app.dart';
+import 'data/services/storage_service.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const ProviderScope(child: App()));
+  final prefs = await SharedPreferences.getInstance();
+  runApp(
+    ProviderScope(
+      overrides: [
+        storageServiceProvider.overrideWithValue(StorageService(prefs)),
+      ],
+      child: const App(),
+    ),
+  );
 }
